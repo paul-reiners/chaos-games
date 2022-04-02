36a104c220f580dbed81c03c104153c87b26b601
paul-reiners <paul.reiners@gmail.com>
2022-04-02 08:55:14 -0500

    Maded compression ratio for hexagons a constant.

--- src/sierpinski_hexagon.py
+++ src/sierpinski_hexagon.py
@@ -4,5 +4,5 @@ from sierpinski_n_gon import sierpinski_m_gon
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
-    compression_ratio = float(sys.argv[2])
+    compression_ratio = 0.333333333
     sierpinski_m_gon(n, 6, '../img/sierpinski_hexagon.png', compression_ratio)
