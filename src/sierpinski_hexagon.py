54bd3d621d8ec27c17585bda566864de4b238784
paul-reiners <paul.reiners@gmail.com>
2022-04-01 19:27:18 -0500

    Cleaned up the code.

--- src/sierpinski_hexagon.py
+++ src/sierpinski_hexagon.py
@@ -1,13 +1,12 @@
 import math
 import random
 import sys
-from itertools import permutations
 
 import matplotlib.pyplot as plt
 
+
 def main(n):
-    indices = [0, 1, 2, 3, 4, 5]
-    v = [[math.sin(indices[i] * 2.0 * math.pi / 6), math.cos(indices[i] * 2.0 * math.pi / 6)] for i in range(6)]
+    v = [[math.sin(i * 2.0 * math.pi / 6), math.cos(i * 2.0 * math.pi / 6)] for i in range(6)]
 
     def w(x, j):
         return [(x[i] + 5 * v[j][i]) / 6.0 for i in range(2)]
@@ -23,6 +22,7 @@ def main(n):
     plt.scatter(xs[4:], ys[4:], 1)
     plt.savefig('../img/sierpinski_hexagon.png')
     plt.show()
+
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
